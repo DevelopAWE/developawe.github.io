0e923c3d2196f6d90e1fe684b6997ee69248a08e
shortnd <ococncol@gmail.com>
2018-03-06 23:20:12 -0500

    added all files for DevelopAWE with Vue

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <Index></Index>
+  <!-- BG -->
+  <div id="bg"></div>
+</div>
+</template>
+
+<script>
+// eslint-disable-next-line
+import Index from "@/components/Index";
+export default {
+  name: 'Wrapper',
+  components: {
+    Index,
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+// import HelloWorld from '@/components/HelloWorld'
+import Wrapper from '@/components/Wrapper';
 
 Vue.use(Router);
 
@@ -8,8 +9,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Wrapper',
+      component: Wrapper,
     },
   ],
+  mode: 'history',
 });
